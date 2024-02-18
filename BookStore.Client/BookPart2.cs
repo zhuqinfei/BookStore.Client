b82f4ef672510f6a87c2e817fa846bca68a7101d
zhuqinfei <1586859762@qq.com>
2024-02-18 11:58:24 +0800

    partial类的使用

---
+++ BookStore.Client/BookPart2.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.Client
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class book
+    {
+       
+        public string report()
+        {
+            return $"#{this.Id} Name:{this.Name} Price:{this.Price}";
+        }
+    }
+}
---
+++ BookStore.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new BookstoreEntities3();
+            var books = dbContext.books;
+            foreach (var book in books)
+            {
+                Console.WriteLine(book.report());
+            }
+        }
+    }
+}
